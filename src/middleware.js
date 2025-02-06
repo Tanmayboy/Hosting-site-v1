6694f32dc6a07d4e6b86a7a4be9cdf33beb78e02
Tanmay <100485745+Tanmayboy@users.noreply.github.com>
2025-02-06 13:10:23 +0530

    V1

---
+++ src/middleware.js
@@ -0,0 +1,18 @@
+import { NextResponse } from "next/server";
+
+export const config = {
+  matcher: "/integrations/:path*",
+};
+
+export function middleware(request) {
+  const requestHeaders = new Headers(request.headers);
+  requestHeaders.set("x-createxyz-project-id", "faa820f9-22fd-4b06-a46b-5813da156de7");
+
+  request.nextUrl.href = `https://www.create.xyz/${request.nextUrl.pathname}`;
+
+  return NextResponse.rewrite(request.nextUrl, {
+    request: {
+      headers: requestHeaders,
+    },
+  });
+}
